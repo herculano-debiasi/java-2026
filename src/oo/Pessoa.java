698df68e32b002ffceefd6fac981b223d7d762a8
Herculano De Biasi <02105018980@udesc.br>
2026-09-11 10:54:25 -0300

    implementação de métodos na classe Pessoa

--- src/oo/Pessoa.java
+++ src/oo/Pessoa.java
@@ -5,4 +5,16 @@ public class Pessoa {
     String nome;
     int idade;
     double peso;
+
+    public void comer() {
+        System.out.printf("Sou %s e estou comendo!\n", nome);
+    }
+
+    public void dormir() {
+        System.out.printf("Sou %s e estou dormindo!\n", nome);
+    }
+
+    public void respirar() {
+        System.out.printf("Sou %s e estou respirando!\n", nome);
+    }
 }
